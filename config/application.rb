f37f868dcc0e7255cecca713e96c6ff1f14b191a
heckfyxe <heckfyxe@gmail.com>
2020-11-23 04:21:25 +0300

    Set timezone and language. Added app_version.rb with application version. Added /about.html and /about/author.html pages. Added models list to README.md

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module TestGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Set timezone
+    config.time_zone = 'Moscow'
+
+    # Set default language
+    I18n.config.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+    I18n.config.default_locale = :ru
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

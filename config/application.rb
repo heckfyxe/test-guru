c22a28c111c31f00c6e441b3a365651e7560528f
heckfyxe <heckfyxe@gmail.com>
2020-11-24 18:21:48 +0300

    Refactored default language config

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,7 @@ module TestGuru
     config.time_zone = 'Moscow'
 
     # Set default language
-    I18n.config.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
-    I18n.config.default_locale = :ru
+    config.i18n.default_locale = :ru
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

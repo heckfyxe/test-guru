c11f12623f7fe493396e3b7b6a0c3a382ee7380f
heckfyxe <heckfyxe@gmail.com>
2020-11-27 15:16:55 +0300

    DB references were deleted. Added not null constraints to some columns.

--- db/migrate/20201126204019_create_answers.rb
+++ db/migrate/20201126204019_create_answers.rb
@@ -2,8 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.string :text, null: false
-      t.boolean :correct, default: false
-      t.references :question, null: false, foreign_key: true
+      t.boolean :correct, null: false, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,9 @@ ActiveRecord::Schema.define(version: 2020_11_26_204019) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
-    t.boolean "correct", default: false
-    t.integer "question_id", null: false
+    t.boolean "correct", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -29,19 +27,15 @@ ActiveRecord::Schema.define(version: 2020_11_26_204019) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text", null: false
-    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", default: 0
-    t.integer "category_id", null: false
+    t.integer "level", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,7 +45,4 @@ ActiveRecord::Schema.define(version: 2020_11_26_204019) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "answers", "questions"
-  add_foreign_key "questions", "tests"
-  add_foreign_key "tests", "categories"
 end

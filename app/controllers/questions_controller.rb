90b37f10db0f818f1866b32fbb8792d57c8fa51e
heckfyxe <heckfyxe@gmail.com>
2020-12-21 08:07:32 +0300

    _question.html.erb question.text is link to question by default. It can be plain text, if pass without_link local. questions#show renders _question.html.erb with without_link=false. _test.html.erb test.questions.length changed to test.questions.size.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,9 +5,7 @@ class QuestionsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
 
-  def show
-    render plain: @question.text
-  end
+  def show; end
 
   def new
     @question = Question.new

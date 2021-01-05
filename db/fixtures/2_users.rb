ec15f2f0bf25d2db7be2f3228660e9bc253366b7
heckfyxe <heckfyxe@gmail.com>
2021-01-05 16:14:45 +0300

    Users' seeds are updated

--- db/fixtures/2_users.rb
+++ db/fixtures/2_users.rb
@@ -1,5 +1,8 @@
-User.seed :first_name, :last_name,
-          { first_name: 'Иван', last_name: 'Соловьёв' },
-          { first_name: 'Игорь', last_name: 'Воронов' },
-          { first_name: 'Егор', last_name: 'Безруков' },
-          { first_name: 'Гарри', last_name: 'Поттер' }
+cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+password_digest = BCrypt::Password.create('password', cost: cost)
+
+User.seed :email,
+          { first_name: 'Иван', last_name: 'Соловьёв', email: 'ivan@mail.com', password_digest: password_digest },
+          { first_name: 'Игорь', last_name: 'Воронов', email: 'igor@mail.com', password_digest: password_digest },
+          { first_name: 'Егор', last_name: 'Безруков', email: 'egor@mail.com', password_digest: password_digest },
+          { first_name: 'Гарри', last_name: 'Поттер', email: 'harry@mail.com', password_digest: password_digest }

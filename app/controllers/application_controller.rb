f46775a0b6c6abc3f94dcd125e5f8da3378cb2f2
heckfyxe <heckfyxe@gmail.com>
2021-01-09 02:34:04 +0300

    Added devise gem. Refactoring for using devise instead previous authentication solution. Added migration for users table to add columns for devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  helper_method :current_user, :logged_in?
-
-  private
-
-  def authenticate_user!
-    return if logged_in?
-
-    cookies[:target_path] = request.path
-    redirect_to login_path
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
 end

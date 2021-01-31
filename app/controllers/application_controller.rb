640074ffc689a30ebd74019c47c123df4e96266b
heckfyxe <heckfyxe@gmail.com>
2021-01-31 09:26:20 +0300

    Fixed after_sign_in_path_for method in application_controller.rb.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    if current_user.is_a? Admin
-      admin_tests_path
-    else
-      stored_location_for(resource) || tests_path
-    end
+    stored_location_for(resource) ||
+      if resource.is_a? Admin
+        admin_tests_path
+      else
+        tests_path
+      end
   end
 end

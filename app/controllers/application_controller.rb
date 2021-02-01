440793a53fcd613e32adbcd0dc8a1486481c810c
heckfyxe <heckfyxe@gmail.com>
2021-02-01 08:25:16 +0300

    Created SessionsController. SessionsController shows welcome alert for new users. Deleted welcome_alert method from ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :welcome_alert, unless: :devise_controller?
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) ||
@@ -18,14 +17,5 @@ class ApplicationController < ActionController::Base
     added_attrs = %i[first_name last_name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  end
-
-  private
-
-  def welcome_alert
-    if current_user && current_user.sign_in_count == 1 && !session[:welcome_alert]
-      flash.now[:alert] = "Привет, #{current_user.first_name}!"
-      session[:welcome_alert] = true
-    end
   end
 end

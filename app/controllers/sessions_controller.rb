977ab5fb97481d6a04d4ac2e3f190494ce002b97
heckfyxe <heckfyxe@gmail.com>
2021-02-01 09:18:33 +0300

    Changed create method in SessionsController.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < Devise::SessionsController
   def create
     super
-    flash[:alert] = "Привет, #{current_user.first_name}!" if current_user && current_user.sign_in_count == 1
+    flash[:alert] = "Привет, #{current_user.first_name}!"
   end
 end
